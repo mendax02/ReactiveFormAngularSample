9e7ad2855aa7d407dabba4b715840eb100bf8a7c
mendax02 <siddharth.rawat@outlook.com>
2020-10-26 18:20:42 +0530

    reactive form validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  lastaddedHobbyIndex = 0;
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
-      'gender': new FormControl('male')
+      'fGroupUserInfo': new FormGroup({
+          'username': new FormControl(null, Validators.required),
+          'email': new FormControl(null, [Validators.required, Validators.email])}),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
 
   }
   onSubmit() {
     console.log(this.signupForm);
+  }
+  addHobby() {
+    const control = new FormControl(null, Validators.required);
+    if (this.lastaddedHobbyIndex > 0 ) {
+      const  hobbiesControls  = this.signupForm.get('hobbies') as FormArray;
+      const lastControlAdded = hobbiesControls.at(this.lastaddedHobbyIndex - 1);
+     // console.log(lastControlAdd);
+      if (!lastControlAdded.invalid) {
+        (<FormArray>this.signupForm.get('hobbies')).push(control);
+      }
+
+    } else {
+      if (this.lastaddedHobbyIndex === 0) {
+        (<FormArray>this.signupForm.get('hobbies')).push(control);
+      }
+    }
+    this.lastaddedHobbyIndex = (<FormArray>this.signupForm.get('hobbies')).length;
+
+    // console.log(this.lastaddedHobbyIndex) ;
   }
 
 }
